0ed42957f6cdd256d0c0a2f96dbb0de373088674
James <james@travelground.com>
2018-08-29 21:16:50 +0200

    Successful API call

--- App.js
+++ App.js
@@ -8,6 +8,7 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import WeatherCard from './Components/WeatherCard';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -18,12 +19,31 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+  constructor(props) {
+    super(props);
+    this.state = {City: "", Condition: "", WC: <Text>Hello</Text>};
+  }
   render() {
+    var that = this;
+    fetch("https://api.aerisapi.com/observations/seattle,wa?client_id=qhM2hWLTX0xOQ9Gsaxl7E&client_secret=gcIxmvPKrF4TEL3FZcYV67unq8eGlawkZXnAZoPI",
+      {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        }
+      }
+    ).then((response) => response.json())
+      .then(function(responseJson){
+        that.setState({WC: <WeatherCard cityName={responseJson.response.place.name} conditions={responseJson.response.ob.weather}/>});
+
+      }).catch((error) => {
+      console.error(error);
+    });
+
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <View>
+        {this.state.WC}
       </View>
     );
   }
--- Components/WeatherCard.js
+++ Components/WeatherCard.js
@@ -9,3 +9,41 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
+export default class WeatherCard extends Component<Props> {
+  render() {
+    return (
+      <View style={styles.card}>
+        <View style={styles.city}>
+          <Text style={styles.cityText}>{this.props.cityName}</Text>
+          <Text style={styles.conditions}>{this.props.conditions}</Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+
+  card: {
+    backgroundColor: '#F5FCFF',
+    width: 300,
+    height: 300,
+    borderRadius: 10
+  },
+
+  city: {
+    top: 10,
+    left: 10
+  },
+
+  cityText: {
+    fontSize: 16,
+    fontWeight: "500"
+  },
+
+  conditions: {
+    fontSize: 14,
+    fontWeight: "300",
+    marginTop: 10
+  }
+});
